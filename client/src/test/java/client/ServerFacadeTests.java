d456030df84d117b9919c4f4f254081044049bdb
magejets <mage.j.lewis@gmail.com>
2025-03-25 15:25:09 -0600

    remaining unit tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,14 +2,15 @@ package client;
 
 import chess.ChessGame;
 import exception.ResponseException;
+import model.GameData;
 import org.junit.jupiter.api.*;
 import request.*;
-import result.CreateResult;
-import result.ListResult;
-import result.LoginResult;
-import result.RegisterResult;
+import result.*;
 import server.Server;
 import server.ServerFacade;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ServerFacadeTests {
@@ -116,24 +117,93 @@ public class ServerFacadeTests {
     }
 
     @Test
-    public void listPositive() {}
+    public void listPositive() {
+        try {
+            RegisterResult registerResult = facade.register(new RegisterRequest("username", "password", "email"));
+            facade.create(new CreateRequest(registerResult.authToken(), "testGame"));
+            facade.create(new CreateRequest(registerResult.authToken(), "testGame2"));
+            List<GameData> expected = new ArrayList<>();
+            expected.add(new GameData(1, null, null, "testGame", new ChessGame()));
+            expected.add(new GameData(2, null, null, "testGame2", new ChessGame()));
+            ListResult result = facade.list(new ListRequest(registerResult.authToken()));
+            Assertions.assertEquals(expected, result.games());
+        } catch (ResponseException e) {
+            Assertions.fail("Response exception");
+        }
+    }
 
     @Test
-    public void listNegative() {}
+    public void listNegative() {
+        try {
+            RegisterResult registerResult = facade.register(new RegisterRequest("username", "password", "email"));
+            facade.create(new CreateRequest(registerResult.authToken(), "testGame"));
+            ListResult result = facade.list(new ListRequest("invalid auth token"));
+        } catch (ResponseException e) {
+            Assertions.assertEquals("Error: unauthorized", e.getMessage());
+        }
+    }
 
     @Test
-    public void joinPositive() {}
+    public void joinPositive() {
+        try {
+            RegisterResult registerResult = facade.register(new RegisterRequest("username", "password", "email"));
+            facade.create(new CreateRequest(registerResult.authToken(), "testGame"));
+            JoinResult result = facade.join(new JoinRequest(registerResult.authToken(), 1, "WHITE"));
+            Assertions.assertNull(result.message());
+            List<GameData> expected = new ArrayList<>();
+            expected.add(new GameData(1, "username", null, "testGame", new ChessGame()));
+            ListResult games = facade.list(new ListRequest(registerResult.authToken()));
+            Assertions.assertEquals(expected, games.games());
+        } catch (ResponseException e) {
+            Assertions.fail("Response Error");
+        }
+    }
 
     @Test
-    public void joinNegative() {}
+    public void joinNegative() {
+        try {
+            RegisterResult registerResult = facade.register(new RegisterRequest("username", "password", "email"));
+            facade.create(new CreateRequest(registerResult.authToken(), "testGame"));
+            JoinResult result = facade.join(new JoinRequest(registerResult.authToken(), 1, "PURPLE"));
+        } catch (ResponseException e) {
+            Assertions.assertEquals("Error: bad request", e.getMessage());
+        }
+    }
 
     @Test
-    public void logoutPositive() {}
+    public void logoutPositive() {
+        try {
+            RegisterResult registerResult = facade.register(new RegisterRequest("username", "password", "email"));
+            LogoutResult result = facade.logout(new LogoutRequest(registerResult.authToken()));
+            Assertions.assertNull(result.message());
+        } catch (ResponseException e) {
+            Assertions.fail("Response Exception");
+        }
+    }
 
     @Test
-    public void logoutNegative() {}
+    public void logoutNegative() {
+        try {
+            RegisterResult registerResult = facade.register(new RegisterRequest("username", "password", "email"));
+            LogoutResult result = facade.logout(new LogoutRequest("invalid auth token"));
+        } catch (ResponseException e) {
+            Assertions.assertEquals("Error: unauthorized", e.getMessage());
+        }
+    }
 
     @Test
-    public void clearPositive() {}
+    public void clearPositive() {
+        try {
+            RegisterResult registerResult = facade.register(new RegisterRequest("username", "password", "email"));
+            facade.create(new CreateRequest(registerResult.authToken(), "testGame"));
+            facade.create(new CreateRequest(registerResult.authToken(), "testGame2"));
+            ClearResult result = facade.clear();
+            registerResult = facade.register(new RegisterRequest("username", "password", "email")); // login again
+            ListResult games = facade.list(new ListRequest(registerResult.authToken()));
+            Assertions.assertEquals(0, games.games().size());
+        } catch (ResponseException e) {
+            Assertions.fail("Response exception");
+        }
+    }
 
 }
